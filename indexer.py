6251aee145a8cb1a277a5b7d7b7e7c7c0b9a7dd2
YanniTheDev <yannithedev@gmail.com>
2024-12-18 15:04:17 +1300

    added schol german

--- indexer.py
+++ indexer.py
@@ -14,7 +14,7 @@ Schol Media Studies 2018 & 2019 & 2020
 Schol Chinese 2018 & 2019 & 2020
 Schol French 2015 & 2016 & 2018 & 2019 & 2020
 Schol German 2014 & 2018 & 2019 & 2020
-Schol Latin 2014 & 2018 & 2019 & 2020
+Schol Latin 2014 & 2016 & 2018 & 2019 & 2020
 ###
 '''
 
